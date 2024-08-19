e80b91170b88c807892774c92b4d888800a8724a
weihongqin <weihongqin@agora.io>
2024-08-19 19:58:29 +0800

    add globals to manage {handle,instance}

--- python_sdk/agora_service/agora_parameter.py
+++ python_sdk/agora_service/agora_parameter.py
@@ -5,11 +5,29 @@ from .agora_base import *
 """
 AGORA_API_C_HDL agora_rtc_conn_get_agora_parameter(AGORA_HANDLE agora_rtc_conn);
 AGORA_API_C_INT agora_parameter_set_parameters(AGORA_HANDLE agora_parameter, const char* json_src);
+AGORA_API_C_INT agora_parameter_get_string(AGORA_HANDLE agora_parameter, const char* key, char* value, uint32_t* value_size);
+"""
+"""
+ut sample:
+jsonparam = "{"key1":"value1}"
+set_parameters(jsonparam)
+
+get_string("key1", value, value_size)
+
+if(value == "value1")
+ PASS:
+else
+FAILED
 """
 
 agora_parameter_set_parameters = agora_lib.agora_parameter_set_parameters
-agora_parameter_set_parameters.argtypes = [ctypes.c_void_p, ctypes.c_char_p]
+agora_parameter_set_parameters.argtypes = [AGORA_HANDLE, ctypes.c_char_p]
 agora_parameter_set_parameters.restype = ctypes.c_int
+
+agora_parameter_get_string = agora_lib.agora_parameter_get_string
+agora_parameter_get_string.argtypes = [AGORA_HANDLE, ctypes.c_char_p, ctypes.c_char_p, ctypes.POINTER(ctypes.c_uint32)]
+agora_parameter_get_string.restype = ctypes.c_int
+
 
 
 class AgoraParameter:
@@ -20,5 +38,14 @@ class AgoraParameter:
         params = ctypes.c_char_p(jsonstr.encode('utf-8'))
         ret = agora_parameter_set_parameters(self.parameter_handle, params)
         return ret
+    
+    #get_string: --- 可以接受的最大buffer size
+    #return: (ret,strvalue, strsize)
+    def get_string(self, key):
+        cdata_key = ctypes.c_char_p(key.encode('utf-8'))
+        value_size = ctypes.c_uint32(128)  #default to 128
+        outbuffer = ctypes.create_string_buffer(value_size)
+        ret = agora_parameter_get_string(self.parameter_handle, key, outbuffer, ctypes.byref(value_size))
+        return ret,outbuffer.value,value_size.value
     def release(self):
         pass
---
+++ python_sdk/agora_service/globals.py
@@ -0,0 +1,57 @@
+import time
+import functools
+import threading 
+
+
+def agorasingleton(cls):
+    """Singleton decorator."""
+    instances = {}
+
+    @functools.wraps(cls)
+    def get_instance(*args, **kwargs):
+        if cls not in instances:
+            instances[cls] = cls(*args, **kwargs)
+        return instances[cls]
+
+    return get_instance
+
+@agorasingleton
+class AgoraHandleInstanceMap:
+    """Global class using singleton pattern."""
+
+    def __init__(self):
+        #for conmap
+        self.con_map = {}
+        self.con_lock = threading.RLock()
+
+        #for local user
+        self.local_user_map = {}
+        self.local_user_lock = threading.RLock()
+        
+    # 使用示例：内部使用with方法来自动获取和释放锁
+    def set_con_map(self, con_handle, con_instance):
+        with self.con_lock:
+            self.con_map[con_handle] = con_instance
+    def get_con_map(self, con_handle):
+        with self.con_lock:
+            return self.con_map.get(con_handle, None)
+        
+    def set_local_user_map(self, local_user_handle, local_user_instance):
+        with self.local_user_lock:
+            self.local_user_map[local_user_handle] = local_user_instance
+
+    def get_local_user_map(self, local_user_handle):
+        with self.local_user_lock:
+            return self.local_user_map.get(local_user_handle, None)
+        
+
+
+# 使用示例
+if __name__ == "__main__":
+    instance1 = AgoraHandleInstanceMap()
+    instance2 = AgoraHandleInstanceMap()
+
+    print(instance1 is instance2)  # 应该输出 True
+
+    instance1.set_data('key', 'value')
+    print(instance2.get_data('key'))  # 应该输出 'value'
