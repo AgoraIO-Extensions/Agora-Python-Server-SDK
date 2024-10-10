5a4389376c94d76466980901e3c85788a7930e39
丁玉松 <dingyusong@agora.io>
2024-10-10 12:02:24 +0800

    feat: update audio encoded send

--- agora_rtc/examples/common/pacer.py
+++ agora_rtc/examples/common/pacer.py
@@ -6,6 +6,8 @@ import time
 # The Pacer method is no longer recommended to consume pcm data. 
 # It is recommended to use AudioConsumer to consume pcm data(or push generated pcm data to rtc)
 import asyncio  
+import logging
+logger = logging.getLogger(__name__)
 
 class Pacer:
     def __init__(self,interval):
@@ -31,8 +33,7 @@ class Pacer:
     async def apace_interval(self, time_interval_s):
         current_time = time.time()
         elapsed_time = current_time - self.last_call_time
+        # logger.info(f"elapsed_time:{elapsed_time}, time_interval_s:{time_interval_s}")
         if elapsed_time < time_interval_s:
-            # time.sleep(time_interval_s - elapsed_time)
             await asyncio.sleep(time_interval_s - elapsed_time)
-            # print("sleep time(ms):", (time_interval_s - elapsed_time)*1000)
         self.last_call_time = time.time()        
--- agora_rtc/examples/example_audio_encoded_send.py
+++ agora_rtc/examples/example_audio_encoded_send.py
@@ -2,17 +2,13 @@
 
 #coding=utf-8
 
-import time
 import os
-import av
+import asyncio
 from common.path_utils import get_log_path_with_filename 
-from common.parse_args import parse_args_example
-from common.pacer import Pacer
-from observer.connection_observer import ExampleConnectionObserver
-from observer.local_user_observer import ExampleLocalUserObserver
-
-from agora.rtc.agora_service import AgoraServiceConfig, AgoraService, RTCConnConfig
-from agora.rtc.audio_encoded_frame_sender import EncodedAudioFrame
+from common.parse_args import parse_args_example, ExampleOptions
+from common.push_audio_encoded_file import push_encoded_audio_from_file
+from common.example_base import RTCBaseProcess
+from agora.rtc.agora_service import AgoraService, LocalUser, RTCConnection
 from agora.rtc.agora_base import *
 import logging
 logging.basicConfig(level=logging.INFO)
@@ -20,86 +16,35 @@ logger = logging.getLogger(__name__)
 
 # run this example
 # python agora_rtc/examples/example_audio_encoded_send.py --appId=xxx --channelId=xxx --audioFile=./test_data/demo.aac
-sample_options = parse_args_example()
-logger.info(f"app_id: {sample_options.app_id}, channel_id: {sample_options.channel_id}, uid: {sample_options.user_id}")
 
-#---------------1. Init SDK
-config = AgoraServiceConfig()
-config.appid = sample_options.app_id
-config.log_path = get_log_path_with_filename(sample_options.channel_id , os.path.splitext(__file__)[0])
-agora_service = AgoraService()
-agora_service.initialize(config)
+class RTCProcessIMPL(RTCBaseProcess):
+    def __init__(self):
+        super().__init__()
+    async def setup_in_connection(self,agora_service:AgoraService, connection:RTCConnection, local_user:LocalUser, sample_options:ExampleOptions):
+        media_node_factory = agora_service.create_media_node_factory()
+        audio_sender = media_node_factory.create_audio_encoded_frame_sender()
+        if not audio_sender:
+            logger.error("create audio sender failed")
+            exit(1)
+        audio_track = agora_service.create_custom_audio_track_encoded(audio_sender, 1)
+        if not audio_track:
+            logger.error("create audio track failed")
+            exit(1)
+        audio_track.set_enabled(1)
+        local_user.publish_audio(audio_track)
+        await self.send(sample_options, audio_sender)        
+        local_user.unpublish_audio(audio_track)
+        audio_track.set_enabled(0)
 
-#---------------2. Create Connection
-con_config = RTCConnConfig(
-    client_role_type=ClientRoleType.CLIENT_ROLE_BROADCASTER,
-    channel_profile=ChannelProfileType.CHANNEL_PROFILE_LIVE_BROADCASTING,
-)
+    async def send(self,sample_options:ExampleOptions, audio_sender):
+        audio_task = asyncio.create_task(push_encoded_audio_from_file(audio_sender, sample_options.audio_file, self._exit))
+        await audio_task
+        # audio_task = asyncio.create_task(push_encoded_audio_from_file(audio_sender, sample_options.audio_file, self._exit))
+        # await push_encoded_audio_from_file(audio_sender, sample_options.audio_file, self._exit)
+        logger.info("send finish")
 
-connection = agora_service.create_rtc_connection(con_config)
-conn_observer = ExampleConnectionObserver()
-connection.register_observer(conn_observer)
-connection.connect(sample_options.token, sample_options.channel_id, sample_options.user_id)
-
-#---------------3. Create Media Sender
-media_node_factory = agora_service.create_media_node_factory()
-audio_sender = media_node_factory.create_audio_encoded_frame_sender()
-if not audio_sender:
-    logger.error("create audio sender failed")
-    exit(1)
-audio_track = agora_service.create_custom_audio_track_encoded(audio_sender, 1)
-if not audio_track:
-    logger.error("create audio track failed")
-    exit(1)
-
-local_user = connection.get_local_user()
-local_user.set_audio_scenario(AudioScenarioType.AUDIO_SCENARIO_CHORUS)
-localuser_observer = ExampleLocalUserObserver()
-local_user.register_local_user_observer(localuser_observer)
-# audio_track.set_max_buffer_audio_frame_number(320*2000)
-
-#---------------4. Send Media Stream
-audio_track.set_enabled(1)
-local_user.publish_audio(audio_track)
-
-def test3(aac_file):
-    sendinterval = 0.1
-    pacer = Pacer(sendinterval)
-    container = av.open(aac_file)
-    sample_rate = 48000
-    channels = 1
-    for stream in container.streams:
-        if stream.type == 'audio':
-            sample_rate = stream.sample_rate
-            channels = stream.channels            
-            logger.info(f"Audio stream: sample rate = {sample_rate}, channels = {channels}")
-            break
-
-    for packet in container.demux():
-        if packet.stream.type == 'audio':
-            logger.info(f"Read audio packet with size {packet.size} bytes, PTS {packet.pts}, DTS {packet.dts}")
-
-            frame = EncodedAudioFrame()
-            frame.speech = 0
-            frame.codec = AudioCodecType.AUDIO_CODEC_AACLC #https://doc.shengwang.cn/api-ref/rtc-server-sdk/cpp/structagora_1_1rtc_1_1_encoded_audio_frame_info.html
-            frame.sample_rate = sample_rate
-            frame.samples_per_channel = 1024
-            frame.number_of_channels = channels
-            frame.send_even_if_empty = 1
-            ret = audio_sender.send_encoded_audio_frame(packet.buffer_ptr, packet.size,frame)
-            time_base = packet.stream.time_base
-            duration_in_seconds = packet.duration * time_base
-            pacer.pace_interval(duration_in_seconds)
-
-for i in range(1):
-    test3(sample_options.audio_file)
-
-# time.sleep(100)
-local_user.unpublish_audio(audio_track)
-audio_track.set_enabled(0)
-connection.unregister_observer()
-connection.disconnect()
-connection.release()
-logger.info("release")
-agora_service.release()
-logger.info("end")
+if __name__ == '__main__':
+    
+    sample_options = parse_args_example()
+    rtc = RTCProcessIMPL()
+    asyncio.run(rtc.run(sample_options, get_log_path_with_filename(sample_options.channel_id,os.path.splitext(__file__)[0])))
