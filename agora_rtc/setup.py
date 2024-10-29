3b4bdcbcbaf71d8b8ca73beb9b47183e17377058
丁玉松 <dingyusong@agora.io>
2024-10-29 18:11:20 +0800

    feat: update version to 2.1.1

--- agora_rtc/setup.py
+++ agora_rtc/setup.py
@@ -45,7 +45,7 @@ class CustomInstallCommand(install):
 
 setup(
     name='agora_python_server_sdk',
-    version='2.1.0',
+    version='2.1.1',
     description='A Python SDK for Agora Server',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
