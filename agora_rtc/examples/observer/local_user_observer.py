1ed8b06efd1268f239f1fbe215aac566acdfd8ad
weihongqin <weihongqin@agora.io>
2024-12-02 20:05:16 +0800

    UPDATE: add onVolumeIndication and onRemotAudioTrackState changed sample

--- agora_rtc/examples/observer/local_user_observer.py
+++ agora_rtc/examples/observer/local_user_observer.py
@@ -17,8 +17,27 @@ class ExampleLocalUserObserver(IRTCLocalUserObserver):
         pass
     
     def on_audio_volume_indication(self, agora_local_user, speakers_list, speaker_number, total_volume):
-        print(f"xxxxxx xxxx on_audio_volume_indication: number = {speaker_number },totoal volume = {total_volume}")
+        #print(f"xxxxxx xxxx on_audio_volume_indication: number = {speaker_number },totoal volume = {total_volume}")
         for i in range(speaker_number):
             speaker = speakers_list[i]
-            print("on vulume indication: ", speaker)
+            #print("on vulume indication: ", speaker)
         pass
+    """
+    # how to define remote user's Mute /UnMute state:
+    for (reason ==5 and state ==0) : remote user is Muted
+    for (reason ==6 and state ==1 ) : remote user is UnMuted
+
+    #on_user_audio_track_state_changed
+    # if reason==5, indicate the remote user is muted by self
+    # if reason==6, indicate the remote user is unmuted by self
+    # an sample for state change activity:
+    # 1. when remote user call MuteAudio: the state is :(will trigger only once)
+    on_user_audio_track_state_changed: user_id=182883922,state=0, reason=5, elapsed=275780
+    # 2. when remote user call UnmuteAudio: the state is : (will trigger twice)
+    on_user_audio_track_state_changed: user_id=182883922,state=1, reason=6, elapsed=275780
+    on_user_audio_track_state_changed: user_id=182883922,state=2, reason=6, elapsed=290281
+    """
+    def on_user_audio_track_state_changed(self, agora_local_user, user_id, agora_remote_audio_track, state, reason, elapsed):
+        print(f"on_user_audio_track_state_changed: user_id={user_id},state={state}, reason={reason}, elapsed={elapsed}")
+        pass
+    
