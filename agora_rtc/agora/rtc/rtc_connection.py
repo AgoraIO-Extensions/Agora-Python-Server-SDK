44ad27bc9ec7ed022cb66c846f57b5cdab07f980
weihongqin <weihongqin@agora.io>
2025-07-23 20:32:56 +0800

    1 add sender track impl;2 add publish api 3 change unreg from pub to private 4 in disconnect to do unregxx auto

--- agora_rtc/agora/rtc/rtc_connection.py
+++ agora_rtc/agora/rtc/rtc_connection.py
@@ -1,6 +1,8 @@
 import time
 import ctypes
+
 from .agora_base import *
+from .agora_service import AgoraService
 from .local_user import LocalUser
 from .rtc_connection_observer import IRTCConnectionObserver
 from ._ctypes_handle._audio_frame_observer import AudioFrameObserverInner
@@ -10,6 +12,11 @@ from ._ctypes_handle._rtc_connection_observer import RTCConnectionObserverInner
 from ._ctypes_handle._ctypes_data import *
 import logging
 logger = logging.getLogger(__name__)
+
+agora_rtc_conn_create = agora_lib.agora_rtc_conn_create
+agora_rtc_conn_create.restype = AGORA_HANDLE
+agora_rtc_conn_create.argtypes = [AGORA_HANDLE, ctypes.POINTER(RTCConnConfigInner)]
+
 
 agora_rtc_conn_get_local_user = agora_lib.agora_rtc_conn_get_local_user
 agora_rtc_conn_get_local_user.restype = AGORA_HANDLE
@@ -59,19 +66,61 @@ agora_rtc_conn_enable_encryption.argtypes = [AGORA_HANDLE, ctypes.c_int, ctypes.
 
 
 class RTCConnection:
-    def __init__(self, conn_handle, is_low_delay: bool = False) -> None:
-        self.conn_handle = conn_handle
+    def __init__(self, service: AgoraService, conn_config: RTCConnConfig, publish_config: RtcConnectionPublishConfig) -> None:
+        self.conn_handle = None
         self.con_observer = None
         self.local_user = None
-        self.local_user_handle = agora_rtc_conn_get_local_user(conn_handle)
+        self.rtc_engine = service
+        #1 create conn_handle
+        self.conn_handle = agora_rtc_conn_create(self.rtc_engine.service_handle, ctypes.byref(RTCConnConfigInner.create(conn_config)))
+        if self.conn_handle is None:
+            return None
+        #2 create local_user
+        self.local_user_handle = agora_rtc_conn_get_local_user(self.conn_handle)
         if self.local_user_handle:
             self.local_user = LocalUser(self.local_user_handle, self)
-        #added to set low delay mode
-        if is_low_delay == True: 
-            self.local_user.set_audio_scenario(AudioScenarioType.AUDIO_SCENARIO_CHORUS)
-        # add to map
-        #AgoraHandleInstanceMap().set_local_user_map(self.conn_handle, self)
-        #AgoraHandleInstanceMap().set_con_map(self.conn_handle, self)
+        #keep publish_config
+        self.publish_config = publish_config
+        #and prepare track and sender for publish
+        self._audio_track = None
+        self._video_track = None
+        self._audio_sender = None
+        self._audio_encoded_sender = None
+        self._video_sender = None
+        self._video_encoded_sender = None
+        self._prepare_publish_track_and_sender()
+        #3 set profile and scenario
+        self.local_user.set_audio_scenario(self.publish_config.audio_scenario)
+        audio_encoder_config = AudioEncoderConfiguration(audioProfile=self.publish_config.audio_profile)
+        self.local_user.set_audio_encoder_configuration(audio_encoder_config)    
+        #4 inner register qios capability observer 
+        #5 prepare senders and tracks for publish, and set track properties like enable, maxbuffersize,mindelay etc
+        #6. create data stream for default 
+        self._data_stream_id = -1
+        self._data_stream_id = self._create_data_stream(False, False)
+        #7. init audio frame observer, video frame observer, video encoded frame observer
+
+    def _prepare_publish_track_and_sender(self)->int:
+        if self.publish_config.is_publish_audio:
+           if self.publish_config.audio_publish_type == AudioPublishType.AUDIO_PUBLISH_TYPE_PCM:
+               self._audio_sender = self.rtc_engine.media_node_factory.create_audio_pcm_data_sender()
+               self._audio_track = self.rtc_engine._create_custom_audio_track_pcm(self._audio_sender, self.publish_config.audio_scenario)
+           elif self.publish_config.audio_publish_type == AudioPublishType.AUDIO_PUBLISH_TYPE_ENCODED_PCM:
+               self._audio_encoded_sender = self.rtc_engine.media_node_factory.create_audio_encoded_data_sender()
+               self._audio_track = self.rtc_engine.create_custom_audio_track_encoded(self.audio_encoded_sender)
+        if self.publish_config.is_publish_video:
+            if self.publish_config.video_publish_type == VideoPublishType.VIDEO_PUBLISH_TYPE_YUV:
+                self._video_sender = self.rtc_engine.media_node_factory.create_video_frame_sender()
+                self._video_track = self.rtc_engine.create_custom_video_track_frame(self._video_sender)
+            elif self.publish_config.video_publish_type == VideoPublishType.VIDEO_PUBLISH_TYPE_ENCODED_IMAGE:
+                self._video_encoded_sender = self.rtc_engine.media_node_factory.create_video_encoded_data_sender()
+                self._video_track = self.rtc_engine.create_custom_video_track_encoded(self._video_encoded_sender)
+        if self._audio_track:
+            self._audio_track.set_enabled(True)
+        if self._video_track:
+            self._video_track.set_enabled(True)
+        return 0
+      
 
     #
     def connect(self, token: str, chan_id: str, user_id: str) -> int:
@@ -80,6 +129,20 @@ class RTCConnection:
 
     #
     def disconnect(self) -> int:
+        #1. unpublish all tracks
+        self.unpublish_audio()
+        self.unpublish_video()
+
+        #2. unregister all observers, but except rtc connection observer
+        self._unregister_audio_frame_observer()
+        self._unregister_video_frame_observer()
+        self._unregister_video_encoded_frame_observer()
+        self._unregister_audio_encoded_frame_observer()
+
+        self._unregister_local_user_observer()
+
+        
+
         ret = agora_rtc_conn_disconnect(self.conn_handle)
         return ret
 
@@ -90,28 +153,20 @@ class RTCConnection:
 
     #
     def register_observer(self, conn_observer: IRTCConnectionObserver) -> int:
+        ret = -1000
         if self.con_observer:
-            self.unregister_observer()
+            self._unregister_observer()
         self.con_observer = RTCConnectionObserverInner(conn_observer, self)
         ret = agora_rtc_conn_register_observer(self.conn_handle, self.con_observer)
         return ret
-
     #
-    def unregister_observer(self) -> int:
+    def _unregister_observer(self) -> int:
         ret = 0
         if self.con_observer:
             ret = agora_rtc_conn_unregister_observer(self.conn_handle)
         self.con_observer = None
         return ret
-
-    # create a data stream
-    def create_data_stream(self, reliable, ordered) -> int:
-        stream_id = ctypes.c_int(0)
-        ret = agora_rtc_conn_create_data_stream(self.conn_handle, ctypes.byref(stream_id), int(reliable), int(ordered))
-        if ret < 0:
-            return None
-        return stream_id.value
-
+    
     # send data stream message to connection
     def send_stream_message(self, stream_id, data) -> int:
         encoded_data = data.encode('utf-8')
@@ -156,9 +211,126 @@ class RTCConnection:
         return agora_rtc_conn_enable_encryption(self.conn_handle, enabled, ctypes.byref(inner_config))
 
     def release(self):
+        # release con observer
+        self._unregister_observer()
+        # release local user
+        self.local_user._release()
         # release local user map
         if self.conn_handle:
             #AgoraHandleInstanceMap().del_local_user_map(self.conn_handle)
             agora_rtc_conn_release(self.conn_handle)
         self.conn_handle = None
         self.local_user = None
+
+        #relese tracks 
+        if self._audio_track:
+            self._audio_track.release()
+            self._audio_track = None
+        if self._video_track:
+            self._video_track.release()
+            self._video_track = None
+        if self._audio_sender:
+            self._audio_sender.release()
+            self._audio_sender = None
+        if self._audio_encoded_sender:
+            self._audio_encoded_sender.release()
+            self._audio_encoded_sender = None
+        if self._video_sender:
+            self._video_sender.release()
+            self._video_sender = None
+        if self._video_encoded_sender:
+            self._video_encoded_sender.release()
+            self._video_encoded_sender = None
+            #release data stream
+        
+        self._data_stream_id = -1
+      
+
+
+    #from verison 2.3.0 new added & modified
+        # create a data stream
+    def _create_data_stream(self, reliable, ordered) -> int:
+        stream_id = ctypes.c_int(0)
+        ret = agora_rtc_conn_create_data_stream(self.conn_handle, ctypes.byref(stream_id), int(reliable), int(ordered))
+        if ret < 0:
+            return None
+        return stream_id.value
+    
+    def register_audio_frame_observer(self, observer,  enable_vad: int, vad_configure) -> int:
+        ret = -1000
+        if self.local_user:
+            ret = self.local_user._register_audio_frame_observer(observer, enable_vad, vad_configure)
+        return ret  
+    def register_local_user_observer(self, observer) -> int:
+        ret = -1000
+        if self.local_user:
+            ret = self.local_user._register_local_user_observer(observer)
+        return ret
+    def register_video_frame_observer(self, observer) -> int:
+        ret = -1000
+        if self.local_user:
+            ret = self.local_user._register_video_frame_observer(observer)
+        return ret
+    def register_video_encoded_frame_observer(self, observer) -> int:   
+        ret = -1000
+        if self.local_user:
+            ret = self.local_user._register_video_encoded_frame_observer(observer)
+        return ret
+    def register_audio_encoded_frame_observer(self, observer) -> int:
+        ret = -1000
+        #todo: need to implement， but not used now
+        return ret
+    def _unregister_audio_frame_observer(self) -> int:
+        ret = -1000
+        if self.local_user:
+            ret = self.local_user._unregister_audio_frame_observer()
+        return ret
+    def _unregister_local_user_observer(self) -> int:
+        ret = -1000
+        if self.local_user:
+            ret = self.local_user._unregister_local_user_observer()
+        return ret
+    def _unregister_video_frame_observer(self) -> int:
+        ret = -1000
+        if self.local_user:
+            ret = self.local_user._unregister_video_frame_observer()
+        return ret
+    def _unregister_video_encoded_frame_observer(self) -> int:
+        ret = -1000
+        if self.local_user:
+            ret = self.local_user._unregister_video_encoded_frame_observer()
+        return ret
+    def _unregister_audio_encoded_frame_observer(self) -> int:
+        ret = -1000
+        #todo: need to implement， but not used now
+        return ret
+    def publish_audio(self)->int:
+        ret = -1000
+        if self.local_user and self._audio_track:
+            ret = self.local_user._publish_audio(self._audio_track)
+        return ret
+    def unpublish_audio(self)->int:
+        ret = -1000
+        if self.local_user and self._audio_track:
+            ret = self.local_user._unpublish_audio(self._audio_track)
+        return ret
+    def publish_video(self)->int:
+        ret = -1000
+        if self.local_user and self._video_track:
+            ret = self.local_user._publish_video(self._video_track)
+        return ret
+    def unpublish_video(self)->int:
+        ret = -1000
+        if self.local_user and self._video_track:
+            ret = self.local_user._unpublish_video(self._video_track)
+        return ret
+    def interrupt_audio(self)->int:
+        ret = -1000
+        if self.local_user is None:
+            ret = -1001
+        elif self.publish_config.audio_scenario == AudioScenarioType.AUDIO_SCENARIO_AI_SERVER:
+            self.unpublish_audio()
+            self.publish_audio()
+        elif self._audio_track:
+            self._audio_track.clear_sender_buffer()
+        return ret
