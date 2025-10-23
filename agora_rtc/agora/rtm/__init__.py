4eadaddf6aa2df19dfdd669b927a55a9d35267d9
weihongqin <weihongqin@agora.io>
2025-10-23 17:57:25 +0800

    mod: only keep loadlibrary related codes

--- agora_rtc/agora/rtm/__init__.py
+++ agora_rtc/agora/rtm/__init__.py
@@ -14,51 +14,21 @@ logger = logging.getLogger(__name__)
 ssl._create_default_https_context = ssl._create_unverified_context
 
 
-def get_file_md5(file_path):
-    hash_md5 = hashlib.md5()
-    with open(file_path, "rb") as f:
-        for chunk in iter(lambda: f.read(4096), b""):
-            hash_md5.update(chunk)
-    return hash_md5.hexdigest()
-
-def get_sdk_path():
-    agora_service_path = os.path.dirname(os.path.abspath(__file__))
-    # change from dir like: /home/xxx/agora_rtc/agora/rtc/agora_sdk to
-    # /home/xxx/agora/agora_sdk
-    # /home/xxx/agora/rtc
-    # /home/xxx/agora/rtm
-    parent_dir = os.path.dirname(agora_service_path)
-    sdk_dir = os.path.join(parent_dir, "agora_sdk")
-    
-    return sdk_dir
-
-
-def _check_download_and_extract_rtm():
-    # just disalbe and manual download it to sdk_dir
-    arch = platform.machine()
-    os_type = platform.system()
-    logger.error(f"arch: {arch}, os_type: {os_type}")
-    pass
-    
-
-
-
-
+from .. import sdk_library_dir, sdk_rtc_dir, sdk_rtm_dir        
 
 #get so path: no need to check and download rtm sdk
 # we have pack the rtm sdk into rtc package
 # so no need to do downlaod and md5 check
-lib_path = get_sdk_path()
-logger.error(f"lib_path: {lib_path}")
+logger.error(f"lib_dir: {sdk_library_dir}")
 
 
 try:
     if sys.platform == 'darwin':
-        lib_agora_rtm_path = os.path.join(lib_path, 'libagora_rtm_sdk_c.dylib')
+        lib_agora_rtm_path = os.path.join(sdk_library_dir, 'libagora_rtm_sdk_c.dylib')
         rtm_lib = ctypes.CDLL(lib_agora_rtm_path)
 
     elif sys.platform == 'linux':
-        lib_agora_rtm_path = os.path.join(lib_path, 'libagora_rtm_sdk_c.so')
+        lib_agora_rtm_path = os.path.join(sdk_library_dir, 'libagora_rtm_sdk_c.so')
         rtm_lib = ctypes.CDLL(lib_agora_rtm_path)
 except OSError as e:
     logger.error(f"Error loading the library: {e}")
